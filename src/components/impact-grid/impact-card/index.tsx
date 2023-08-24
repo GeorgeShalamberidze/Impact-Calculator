c61748da3b97ea3da7d987a2e5e71b3e69ed1fbe
GIORGISHALAMBER\G.Shalamberidze <g.shalamberidze@tnet.ge>
2023-08-25 02:33:04 +0400

    grid

---
+++ src/components/impact-grid/impact-card/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ImpactCard = () => {
+	return (
+		<div
+			style={{ width: '230px', height: '280px', backgroundColor: 'gray' }}
+			className="flex justify-center items-center"
+		>
+			qwe
+		</div>
+	);
+};
+
+export default ImpactCard;
--- src/components/impact-grid/index.tsx
+++ src/components/impact-grid/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import ImpactCard from './impact-card';
+import './style.css';
 
 const ImpactGrid = () => {
-	return <div>ImpactGrid</div>;
+	return (
+		<div className="grid md:grid-cols-3 sm:grid-cols-2 gap-5 pt-[30px] pr-[30px] pb-[35px] pl-[40px] border border-red-500 justify-center items-center">
+			<ImpactCard />
+			<ImpactCard />
+			<ImpactCard />
+			<ImpactCard />
+			<ImpactCard />
+			<ImpactCard />
+			<ImpactCard />
+		</div>
+	);
 };
 
 export default ImpactGrid;
